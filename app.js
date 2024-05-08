9e96f01fd19f0848d4465f8ff7adbba95fa2cc18
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-05-08 11:24:41 +0100

    other routes added

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 import express from "express";
-import { getRoutes } from "./models/routes.js";
+import {
+  getRoutes,
+  getRouteById,
+  saveNewRoute,
+  deleteRoute,
+} from "./models/routes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
 
+// 1_ GET all routes (to display in table on FrontEnd (FE)) ---- /getAllRoutes
+// GET to retrieve stored route
+// read row from database
+// return the route data and name -- as JSON?
 app.get("/routes", async (req, res) => {
   const allRoutes = await getRoutes();
   res.status(200).json({ status: "success", payload: allRoutes });
   //   console.log(allRoutes);
+});
+
+// 2_ GET route by ID to display single route on user click ---- /getRouteById
+app.get("/route/:id", async (req, res) => {
+  // get id from params in url
+  const id = req.params.id;
+  //   pass id to function to get specificed route
+  const route = await getRouteById(id);
+  //   return chosen route
+  res.status(200).json({ status: "success", payload: route });
+});
+
+// POST to store/save a route
+app.post("/newRoute", async (req, res) => {
+  //define data from request body - THIS WILL NEED VALIDATING!
+  const route = req.body;
+  // add new route to DB once validated
+  const newRoute = await saveNewRoute(route);
+  // return new route and success
+  res.status(201).json({ status: "success", payload: newRoute });
+});
+
+// DELETE to delete a saved route
+app.delete("/delete/:id", async (req, res) => {
+  // get id from params in url
+  const id = req.params.id;
+  // pass id to delete function
+  const deletedRoute = deleteRoute(id);
+  // return details of deleted route
+  res.status(200).json({ status: "success", payload: "Deleted Successfully" });
 });
 
 app.listen(PORT, (error) => {
--- models/routes.js
+++ models/routes.js
@@ -9,3 +9,34 @@ export async function getRoutes() {
   // send to db
   return result.rows;
 }
+
+// GET route by ID
+export async function getRouteById(id) {
+  // Query DB and return matching record by id
+  const queryText = "SELECT * FROM routes WHERE id =$1";
+  // Pass query to DB
+  const result = await pool.query(queryText, [id]);
+  return result.rows[0];
+}
+
+// POST new route
+export async function saveNewRoute(route) {
+  //   console.log(route);
+  //define SQL query for new route
+  const queryText =
+    "INSERT INTO routes (route_name, route_data) VALUES ($1, $2) RETURNING *";
+  // take values from req body
+  const values = [route.name, route.data];
+  //Send query to DB
+  const result = await pool.query(queryText, values);
+  // return result (newly added route)
+  return result.rows[0];
+}
+
+// DELETE route
+export async function deleteRoute(id) {
+  // DELETE FROM routes WHERE id = 2
+  const queryText = "DELETE FROM routes WHERE id = $1";
+  const result = await pool.query(queryText, [id]);
+  return result.rows[0];
+}
